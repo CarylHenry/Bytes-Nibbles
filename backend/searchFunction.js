9b18fb7533101f82c22bd6a951bd10baefb2e53e
kongdai123 <tianfuwang2021@u.northwestern.edu>
2019-01-21 12:20:22 -0600

    Search by name
    Search for restaurant by name and returns a message to check if the resurant is opened NOW

--- backend/searchFunction.js
+++ backend/searchFunction.js
@@ -1,6 +1,6 @@
 function searchFunction(){
   var q = document.getElementById("search").value;
-  var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurant&location=" + q ;
+  var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurant&location=evanston&open_now=true" ;
   $.ajax({
      url: myurl,
      headers: {
@@ -18,5 +18,50 @@ function searchFunction(){
         alert("no results found");
       }
      }
+   });
+}
+
+function searchByName() {
+  var q = document.getElementById("search").value;
+  var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/autocomplete?text=" + q +"&latitude=42.047719&longitude=-87.683712";
+  $.ajax({
+     url: myurl,
+     headers: {
+      'Authorization':'Bearer 7-uNealg5uVCOofbvSItfxosg8aoTHPS7ZmsjqyP12Va7SyKQdgt8lII8_qeVIDe7Ibcz7Z93RfMwyz5xVArMPb6tejoT_fuWrBUwn0QCOtiJkaQwaY2sLNTGizuW3Yx',
+  },
+  method: 'GET',
+  dataType: 'json',
+  success: function(data){
+     if(data.businesses == ""){
+       alert("no result found");
+       return;
+     }
+     var i = 0;
+     for (i; i < data.businesses.length; i++){
+     var id = data.businesses[i].id;
+     var name = data.businesses[i].name;
+     alert("found " + name);
+     searchById(id);
+   }
+   }
+  });
+}
+
+function searchById(id) {
+   var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id;
+   $.ajax({
+      url: myurl,
+      headers: {
+       'Authorization':'Bearer 7-uNealg5uVCOofbvSItfxosg8aoTHPS7ZmsjqyP12Va7SyKQdgt8lII8_qeVIDe7Ibcz7Z93RfMwyz5xVArMPb6tejoT_fuWrBUwn0QCOtiJkaQwaY2sLNTGizuW3Yx',
+   },
+   method: 'GET',
+   dataType: 'json',
+   success: function(data){
+     if (data.is_closed == false) {
+      alert(data.name + " is opened now!");}
+     else {
+       alert(data.name + " is closed and will open at " + data.hours.open[0].start);
+     }
+    }
    });
 }
